be49f1230574c5420200baf9f61366f7a0e6e9a7
Luis Santos <luis.eugenio.santos@accenture.com>
2020-07-23 14:23:00 -0500

    First Commit

---
+++ src/app.js
@@ -0,0 +1,16 @@
+const fs = require("fs")
+const path = require("path")
+const express = require("express")
+
+
+const app = express()
+
+app.use(express.static('public'));
+app.set('views', path.join(__dirname+'/views'))
+app.set('view engine', 'ejs')
+
+app.get('/', (req,res) => {
+    res.render('index')
+})
+
+app.listen(8080)
