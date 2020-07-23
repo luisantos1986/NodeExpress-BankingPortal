4194133879002b0260a8e61d5d8a1bfa27a143e7
Luis Santos <luis.eugenio.santos@accenture.com>
2020-07-23 15:09:40 -0500

    Finishing module1

--- src/app.js
+++ src/app.js
@@ -5,12 +5,14 @@ const express = require("express")
 
 const app = express()
 
-app.use(express.static('public'));
+app.use('/',express.static(__dirname+'/public'));
 app.set('views', path.join(__dirname+'/views'))
 app.set('view engine', 'ejs')
 
 app.get('/', (req,res) => {
-    res.render('index')
+    res.render('index', {title: "Index"})
 })
 
-app.listen(8080)
+app.listen(3000, () => {
+    console.log('PS Project Running on port 3000!')
+})
