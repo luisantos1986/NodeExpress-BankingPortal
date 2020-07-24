5c5f8f0fc9a09e49e75ce4eb9013d50c34f8cc88
Luis Santos <luis.eugenio.santos@accenture.com>
2020-07-23 20:44:27 -0500

    too much ejs

--- src/app.js
+++ src/app.js
@@ -2,7 +2,10 @@ const fs = require("fs")
 const path = require("path")
 const express = require("express")
 
-
+const accountData = fs.readFileSync(__dirname+'/json/accounts.json', 'utf-8')
+const accounts = JSON.parse(accountData)
+const userData = fs.readFileSync(__dirname+'/json/users.json', 'utf-8')
+const users = JSON.parse(userData)
 const app = express()
 
 app.use('/',express.static(__dirname+'/public'));
@@ -10,7 +13,11 @@ app.set('views', path.join(__dirname+'/views'))
 app.set('view engine', 'ejs')
 
 app.get('/', (req,res) => {
-    res.render('index', {title: "Index"})
+    res.render('index', {title: "Index", accounts: accounts})
+})
+
+app.get('/savings', (req,res) => {
+    res.render('accounts', {account: accounts.savings})
 })
 
 app.listen(3000, () => {
