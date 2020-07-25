adda253409ad6b3597958439d289dd4d0045aede
Luis Santos <luis.eugenio.santos@accenture.com>
2020-07-25 15:14:44 -0500

    finishing module 3

--- src/app.js
+++ src/app.js
@@ -33,9 +33,10 @@ app.post('/transfer', (req,res) => {
     const accountsJSON = JSON.stringify(accounts, null, 4);
     fs.writeFileSync(path.join(__dirname, 'json', 'accounts.json'), accountsJSON, 'utf8');
     res.render('transfer', {message: 'Transfer Completed'});
-})
+});
 
 app.get('/payment', (req,res) => res.render('payment', {account: accounts.credit}));
+
 app.post('/payment', (req,res) => {
     accounts.credit.balance -= req.body.amount;
     accounts.credit.available += parseInt(req.body.amount, 10)
